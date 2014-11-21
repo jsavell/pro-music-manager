4d5184161ab7cd38bdbe22a8eb614de29a6e4906
jsavell <contact@shadowater.com>
2014-11-20 21:49:14 -0600

    use filename variable for config loading

--- _application/config/config.php
+++ _application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 $filename = dirname(__FILE__)."/config.json";
 if (is_file($filename)) {
-	$config = json_decode(file_get_contents(dirname(__FILE__)."/config.json"),true);
+	$config = json_decode(file_get_contents($filename),true);
 } else {
 	echo "Couldn't find the config file!";
 	die();
