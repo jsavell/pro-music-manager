0131089877e31a891d779d5787f42481cfbff8bd
jsavell <contact@shadowater.com>
2014-12-09 20:04:51 -0600

    (wip) start of generic file upload view, define sales import control

--- _application/controllers/imports.control.php
+++ _application/controllers/imports.control.php
@@ -1,13 +1,25 @@
 <?php
 $page['title'] = 'Import Data';
 $page['navigation'] = array(
-						array("name"=>"sales","action"=>"sales","modal"=>false));
+						array("name"=>"sales","action"=>"sales","modal"=>true));
 $page['search'] = false;
 
 if (isset($data['action'])) {
 	switch ($data['action']) {
 		case 'sales':
 			$page['subtitle'] = 'Import Sales';
+			if (!empty($data['subaction'])) {
+				switch ($data['subaction']) {
+					case 'process':
+						//todo: pass the data off to the model
+					break;
+					case 'configure':
+						//todo: have the user define the correspondence between db field name and csv column
+					break;
+				}
+			} else {
+				$viewfile = 'imports.files.new.view.php';
+			}
 		break;
 	}
 }
