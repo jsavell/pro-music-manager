a98113aa67c6f009bd9ddbae231ea36975d08875
jsavell <contact@shadowater.com>
2015-02-05 20:21:50 -0600

    (wip) upload and view sales csv

--- _application/controllers/imports.control.php
+++ _application/controllers/imports.control.php
@@ -15,6 +15,45 @@ if (isset($data['action'])) {
 					break;
 					case 'configure':
 						//todo: have the user define the correspondence between db field name and csv column
+						$salesFields = array('date','track','version','library','total','payout');
+						$fieldCount = count($salesFields);
+						if (($handle = fopen($_FILES['fileupload']['tmp_name'], "r")) !== FALSE) {
+							$csvData = array();
+							$rowCount = 0;
+						    while (($csvRow = fgetcsv($handle, 1000, ",")) !== FALSE) {
+						        $num = count($csvRow);
+						        for ($c=0; $c < $num; $c++) {
+						            $out .= "<td>{$data[$c]}</td>";
+									$csvData[$rowCount][$c] = $csvRow[$c]; 
+						        }
+								$rowCount++;
+						    }
+
+
+							$out .= '<table class="list">
+										<tr>';
+							for ($x=0;$x<$num;$x++) {
+								$out .= "	<td>
+												<select class=\"capitalize\" name=\"field{$x}\">
+													<option value=\"\"></option>";
+								foreach ($salesFields as $field) {
+									$out .= "		<option value=\"{$field}\">{$field}</option>";
+								}
+								$out .= '		</select>
+											</td>';
+							}
+							$out .= '</tr>';
+						    foreach ($csvData as $row) {
+								$out .= '<tr>';
+						        $num = count($row);
+						        foreach ($row as $value) {
+						            $out .= "<td>{$value}</td>";
+						        }
+								$out .= '</tr>';
+						    }
+							$out .= '</table>';
+						    fclose($handle);
+						}
 					break;
 				}
 			} else {
--- _application/views/imports.files.new.view.php
+++ _application/views/imports.files.new.view.php
@@ -1,3 +1,10 @@
 <?php
-$out .= '<div>new file!</div>';
+$out .= '<div>
+			<form id="importFile" name="importFile" method="POST" action="'.$app_http.'" enctype="multipart/form-data">
+				<input type="hidden" name="action" value="sales" />
+				<input type="hidden" name="subaction" value="configure" />
+				<input type="file" name="fileupload" />
+				<input type="submit" name="submit_import" value="Upload File" />
+			</form>
+		</div>';
 ?>
