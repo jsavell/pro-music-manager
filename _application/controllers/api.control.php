e03d3a3c6ec654629d2f584124d96fdca08e4b56
jsavell <contact@shadowater.com>
2020-04-25 10:53:10 -0500

    retrieve genres over api

--- _application/controllers/api.control.php
+++ _application/controllers/api.control.php
@@ -1,20 +1,28 @@
 <?php
 $data['json'] = 1;
 
-if (!empty($data['data']) && $data['data'] == 'tracks') {
-	$sortBy = null;
-	if (!empty($data['sort'])) {
-		$sortBy = $data['sort'];
-	}
+$cgenres = new genres();
 
-	$ctracks = new tracks();
-	$cgenres = new genres();
-		
-	$tracks = $ctracks->getTracks($sortBy);
-	$genres = $cgenres->getGenres();
-	foreach ($tracks as &$track) {
-		$track['genre'] = $genres[$track['genreid']]['name'];
+if (!empty($data['data'])) {
+	switch ($data['data']) {
+		case 'tracks':
+			$sortBy = null;
+			if (!empty($data['sort'])) {
+				$sortBy = $data['sort'];
+			}
+
+			$ctracks = new tracks();
+
+			$tracks = $ctracks->getTracks($sortBy);
+			$genres = $cgenres->getGenres();
+			foreach ($tracks as &$track) {
+				$track['genre'] = $genres[$track['genreid']]['name'];
+			}
+			$out = json_encode($tracks);
+		break;
+		case 'genres':
+			$out = json_encode($cgenres->getGenresDetailed());
+		break;
 	}
-	$out = json_encode($tracks);
 }
 ?>
