2e1a37cd25635401507a10af84e2eb4e510b1400
jsavell <contact@shadowater.com>
2020-07-11 15:44:45 -0500

    remove json config reference

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 $configBase = "./App/Config/";
-if (!is_file($configBase.'config.json')) {
-	echo 'Make sure to create and configure the config file!';
-} else {
-	include $configBase.'config.php';
-	$forceRedirectUrl = "{$config['PATH_HTTP']}login.php";
-	include PATH_LIB."loader.php";
-}
+include $configBase.'config.php';
+$forceRedirectUrl = "{$config['PATH_HTTP']}login.php";
+include PATH_LIB."loader.php";
+
 ?>
