38a57533bb3de0151add9ce3973668e50c145f84
jsavell <contact@shadowater.com>
2020-07-11 15:40:12 -0500

    remove errant echo

--- App/Classes/Data/User.php
+++ App/Classes/Data/User.php
@@ -6,7 +6,6 @@ class User extends CoreData\UserDB {
 	public function logIn($username,$password) {
 		session_regenerate_id(true);
 		$sql = "SELECT id,password FROM {$this->primaryTable} WHERE username=:username";
-		echo $sql;
 		if ($result = $this->executeQuery($sql,array(":username"=>$username))) {
 			if (password_verify($password,$result[0]['password'])) {
 				$this->setSessionUserId($result[0]['id']);
